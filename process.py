870646fc7b5f51183b54e6319520c93b03d08361
danieljohnevans <djevan11@gmail.com>
2023-02-28 14:48:19 -0600

    writing for multiple files

--- process.py
+++ process.py
@@ -1,4 +1,5 @@
 from booknlp.booknlp import BookNLP
+import os
 
 model_params={
 		"pipeline":"entity,quote,supersense,event,coref", 
@@ -8,12 +9,21 @@ model_params={
 booknlp=BookNLP("en", model_params)
 
 # Input file to process
-input_file="input_dir/pg70123.txt"
 
-# Output directory to store resulting files in
-output_directory="output_dir/test/"
+all_files = os.listdir('input_dir')
+# input_file="input_dir/pg70123.txt"
 
-# File within this directory will be named ${book_id}.entities, ${book_id}.tokens, etc.
-book_id="pg70123"
+# # Output directory to store resulting files in
+# # File within this directory will be named ${book_id}.entities, ${book_id}.tokens, etc.
+input_dir=[]
+output_directory=[]
+book_id = []
+for i in all_files:
+    input_dir.append("input_dir/" + i)
+    output_directory.append("output_dir/" + i)
+    book_id.append(i.strip(".txt"))
 
-booknlp.process(input_file, output_directory, book_id)
+for i in range(len(input_dir)):
+    booknlp.process(input_dir[i], output_directory[i], book_id[i])
+
+# booknlp.process(all_files, output_directory, book_id)
